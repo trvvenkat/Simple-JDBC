6f07c2bedae475e5d93ff4bf678485570cf29308
Trainee <Trainee@KGFSL.COM>
2018-03-09 12:14:58 +0530

    Simple JDBC web app

---
+++ src/main/java/Register.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/register")
+public class Register extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        try{
+        
+        //loading drivers for mysql
+        Class.forName("com.mysql.jdbc.Driver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:mysql://localhost:3306/dummy","root","");
+
+        PreparedStatement ps=con.prepareStatement
+                  ("insert into student values(?,?,?)");
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, pass);
+        int i=ps.executeUpdate();
+        
+          if(i>0)
+          {
+            out.println("You are sucessfully registered");
+          }
+          else
+          {
+            out.println("registration failed");
+          }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+	
+      }
+  }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Register form</title>
+    </head>
+    <body>
+        <form method="post" action="/register">
+        Name:<input type="text" name="name" /><br/>
+        Email ID:<input type="text" name="email" /><br/>
+        Password:<input type="text" name="pass" /><br/>
+        <input type="submit" value="register" />
+        </form>
+    </body>
+</html>
